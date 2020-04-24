9f71b39621395ce6b28042434b9a6aca700ea66d
Diego M. Rodrigues <diegomr86@gmail.com>
2020-04-24 00:32:09 -0300

    Adiciona challenge do certbot

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ var router = require('express').Router();
 
 router.use('/api', require('./api'));
 
-router.get('/.well-known/acme-challenge/343fzCmsCO3gOFr2HVxRwtbQLxliZLEpsgiyY3p-oIc', function(req, res) {
-  res.send('343fzCmsCO3gOFr2HVxRwtbQLxliZLEpsgiyY3p-oIc.0XMFlq6Y8YnP-dipKWnStwpqYcDSozaJeVDIUUqOAV0');
+router.get('/.well-known/acme-challenge/Ww7ZEJD5DDj6HszhmcX-YX-os83YXoTLiZBGxbUGh3g', function(req, res) {
+  res.send('Ww7ZEJD5DDj6HszhmcX-YX-os83YXoTLiZBGxbUGh3g.B4nxrO1S1B3e7HDu2IusPKp4H-N5oOFGWoTd_E4YLvg');
 });
 
 router.get('/painel', function(req, res, next) {
