938b9fe3d7fc50a3c9974d778af6e90fcd9e5e49
Diego M. Rodrigues <diegomr86@gmail.com>
2020-04-27 23:15:37 -0300

    Altera estratégia de login com facebook

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,10 +8,13 @@ passport.use(new LocalStrategy({
   passwordField: 'password'
 }, function(email, password, done) {
   User.findOne({ email: email}).then(function(user){
-    if((!user || !user.validPassword(password)) && user.facebook_id != password){
-      return done(null, false, {errors: {'email or password': 'is invalid'}});
+    if(user && user.validPassword(password)){
+      return done(null, user);
+    }
+    if(user && user.facebook_id+"" == password+""){
+      return done(null, user);
     }
 
-    return done(null, user);
+    return done(null, false, {errors: {'email or password': 'is invalid'}});
   }).catch(done);
 }));
